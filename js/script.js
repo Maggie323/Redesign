3ed220427cf783f3783e285303a3b8557fc971b0
Maggie323 <la_magaly92@yahoo.com>
2018-05-27 21:50:58 -0700

    two charts are working -demo charts

--- js/script.js
+++ js/script.js
@@ -1,33 +1,74 @@
 
-      // Load the Visualization API and the corechart package.
-      google.charts.load('current', {'packages':['corechart']});
+// Load the Visualization API and the corechart package.
+google.charts.load('current', {'packages':['corechart']});
 
-      // Set a callback to run when the Google Visualization API is loaded.
-      google.charts.setOnLoadCallback(drawChart);
 
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
-      function drawChart() {
+// --- ^ Only have to do this once!
+// For second chart:
+// 1. Seta  callback for it
+// 2. Have the callback function for it (setOnLoadCallback).
+// 3. Be sure all callback functions have different names
+// 4. Be sure all callback functions use different divs to draw in. Add a new div to HTML for each chart
 
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Topping');
-        data.addColumn('number', 'Slices');
-        data.addRows([
-          ['Mushrooms', 3],
-          ['Onions', 1],
-          ['Olives', 1],
-          ['Zucchini', 1],
-          ['Pepperoni', 2]
-        ]);
 
-        // Set chart options
-        var options = {'title':'How Much Pizzzza I Ate Last Night',
-                       'width':400,
-                       'height':300};
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawChart1);
+google.charts.setOnLoadCallback(drawChart2);
 
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart1() {
+
+	// Create the data table.
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'Topping');
+	data.addColumn('number', 'Slices');
+	data.addRows([
+		['Mushrooms', 3],
+		['Onions', 1],
+		['Olives', 1],
+		['Zucchini', 1],
+		['Pepperoni', 2]
+	]);
+
+	// Set chart options
+	var options = {'title':'How Much Pizzzza I Ate Last Night',
+								 'width':400,
+								 'height':300,
+									'legend': 'left',
+								 'is3D': true
+								};
+
+	// Instantiate and draw our chart, passing in some options.
+	var chart = new google.visualization.PieChart(document.getElementById('chart_div1'));
+	chart.draw(data, options);
+}
+
+function drawChart2() {
+
+	// Create the data table.
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'Topping');
+	data.addColumn('number', 'Slices');
+	data.addRows([
+		['Mushrooms', 3],
+		['Onions', 1],
+		['Olives', 1],
+		['Zucchini', 1],
+		['Pepperoni', 2]
+	]);
+
+	// Set chart options
+	var options = {'title':'How Much Pizzzza I Ate Last Night',
+								 'width':400,
+								 'height':300,
+									'legend': 'left',
+								 'is3D': true
+								};
+
+	// Instantiate and draw our chart, passing in some options.
+	var chart = new google.visualization.PieChart(document.getElementById('chart_div2'));
+	chart.draw(data, options);
+}
